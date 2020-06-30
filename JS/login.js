b072608c7161ff714cdfb29e2fc4e294e5a04cc6
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2020-06-30 10:51:57 +0700

    fix bug login

--- JS/login.js
+++ JS/login.js
@@ -1,9 +1,9 @@
 const DEFAULT_USERNAME = "admin";
-const DEFAULT_PASSWORD = "123456";
+const DEFAULT_PASSWORD = "123456a";
 function login() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
-    if (username !== DEFAULT_USERNAME && password !== DEFAULT_PASSWORD){
+    if (username !== DEFAULT_USERNAME || password !== DEFAULT_PASSWORD){
         alert("Đăng nhập thất bại, mời đăng nhập lại!");
         return false;
     }else if (username === DEFAULT_USERNAME && password === DEFAULT_PASSWORD){
