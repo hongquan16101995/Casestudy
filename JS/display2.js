e5a90ac7363880b95c213e402af2d5ea7a5a85f7
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2020-07-01 10:59:19 +0700

    deploy

--- JS/display2.js
+++ JS/display2.js
@@ -1,4 +1,4 @@
-let hondaClass = function(image, price, name){
+let Honda = function(image, price, name){
     this.image = image;
     this.price = price;
     this.name = name;
@@ -21,12 +21,12 @@ function ShowElement (array){
 
 
 //danh sách oto
-let car1 = new hondaClass("../car_moto/car1.png", 418000000, "BRIO");
-let car2 = new hondaClass("../car_moto/car2.png", 559000000, "CITY");
-let car3 = new hondaClass("../car_moto/car3.png", 729000000, "CIVIC");
-let car4 = new hondaClass("../car_moto/car4.png", 786000000, "HR-V");
-let car5 = new hondaClass("../car_moto/car5.png", 983000000, "CR-V");
-let car6 = new hondaClass("../car_moto/car6.png", 1319000000, "ACCORD");
+let car1 = new Honda("../car_moto/car1.png", 418000000, "BRIO");
+let car2 = new Honda("../car_moto/car2.png", 559000000, "CITY");
+let car3 = new Honda("../car_moto/car3.png", 729000000, "CIVIC");
+let car4 = new Honda("../car_moto/car4.png", 786000000, "HR-V");
+let car5 = new Honda("../car_moto/car5.png", 983000000, "CR-V");
+let car6 = new Honda("../car_moto/car6.png", 1319000000, "ACCORD");
 
 let arrayCar = [[car1,car2,car3],[car4,car5,car6]];
 
@@ -36,12 +36,12 @@ function showCar() {
 
 
 //list xe ga
-let scooter1 = new hondaClass("../car_moto/xega1.png", 71000000, "SH 150i");
-let scooter2 = new hondaClass("../car_moto/xega2.png", 56500000, "PCX 150");
-let scooter3 = new hondaClass("../car_moto/xega3.png", 41200000, "AirBlade 125");
-let scooter4 = new hondaClass("../car_moto/xega4.png", 30000000, "Vision");
-let scooter5 = new hondaClass("../car_moto/xega5.png", 38000000, "Lead");
-let scooter6 = new hondaClass("../car_moto/xega6.png", 90000000, "PCX Hybrid");
+let scooter1 = new Honda("../car_moto/xega1.png", 71000000, "SH 150i");
+let scooter2 = new Honda("../car_moto/xega2.png", 56500000, "PCX 150");
+let scooter3 = new Honda("../car_moto/xega3.png", 41200000, "AirBlade 125");
+let scooter4 = new Honda("../car_moto/xega4.png", 30000000, "Vision");
+let scooter5 = new Honda("../car_moto/xega5.png", 38000000, "Lead");
+let scooter6 = new Honda("../car_moto/xega6.png", 90000000, "PCX Hybrid");
 
 let arrayScooter = [[scooter1,scooter2,scooter3],[scooter4,scooter5,scooter6]];
 
@@ -51,11 +51,11 @@ function showScooter() {
 
 
 //list xe motor
-let motor1 = new hondaClass("../car_moto/xemoto1.png", 678000000, "CBR1000RR");
-let motor2 = new hondaClass("../car_moto/xemoto2.png", 678000000, "CBR1000RR");
-let motor3 = new hondaClass("../car_moto/xemoto3.png", 678000000, "CBR1000RR");
-let motor4 = new hondaClass("../car_moto/xemoto4.png", 678000000, "CBR1000RR");
-let motor5 = new hondaClass("../car_moto/xemoto5.png", 678000000, "CBR1000RR");
+let motor1 = new Honda("../car_moto/xemoto1.png", 678000000, "CBR1000RR");
+let motor2 = new Honda("../car_moto/xemoto2.png", 678000000, "CBR1000RR");
+let motor3 = new Honda("../car_moto/xemoto3.png", 678000000, "CBR1000RR");
+let motor4 = new Honda("../car_moto/xemoto4.png", 678000000, "CBR1000RR");
+let motor5 = new Honda("../car_moto/xemoto5.png", 678000000, "CBR1000RR");
 
 let arrayMotor = [[motor1,motor2,motor3],[motor4,motor5]];
 
@@ -65,10 +65,10 @@ function showMotor() {
 
 
 //list xe số
-let motorbike1 = new hondaClass("../car_moto/xeso1.png", 30200000, "Future 125FI");
-let motorbike2 = new hondaClass("../car_moto/xeso2.png", 18800000, "Blade");
-let motorbike3 = new hondaClass("../car_moto/xeso3.png", 17800000, "Wave Alpha");
-let motorbike4 = new hondaClass("../car_moto/xeso4.png", 21700000, "Wave RSX FI");
+let motorbike1 = new Honda("../car_moto/xeso1.png", 30200000, "Future 125FI");
+let motorbike2 = new Honda("../car_moto/xeso2.png", 18800000, "Blade");
+let motorbike3 = new Honda("../car_moto/xeso3.png", 17800000, "Wave Alpha");
+let motorbike4 = new Honda("../car_moto/xeso4.png", 21700000, "Wave RSX FI");
 
 let arrayMotorbike = [[motorbike1,motorbike2,motorbike3],[motorbike4]];
 
